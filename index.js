ae64657e8abec1a71e8e10e050c2c46b9181276d
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2022-12-23 09:01:09 -0600

    add copy function and render html

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const selectOptionColor = document.querySelector('#select-color-option')
 const getColorBtn = document.querySelector('#get-color-btn')
 const colorContainer = document.querySelector('#color-container')
 const color = document.querySelector('#color')
+const input = document.querySelector("input.copyfrom"); 
 
-
-getColorBtn.addEventListener('click', () => {
+const renderMenu = ()=> {
     let colorHtml = ''
     const option = selectOptionColor.options[selectOptionColor.selectedIndex].value
-    
     fetch(
         `https://www.thecolorapi.com/scheme?hex=${color.value.slice(1)}&mode=${option}`
     )
@@ -19,11 +18,11 @@ getColorBtn.addEventListener('click', () => {
             
             colorHtml += `
                 <div class="hex-container tooltip" id="copy-area">
-                    <span class="tooltiptext" id="myTooltip">${newColor}</span>
+                    <span class="tooltiptext" data-tooltip="${newColor}">${newColor}</span>
                     <div class="rectangle-shape color-one" 
                         id="rectangle-shape" style="background:${newColor}">
                     </div>
-                    <p class="hex" id="hex">${newColor}</p>
+                    <p class="hex" data-color="${newColor}">${newColor}</p>
                 </div>
                 
             `
@@ -31,36 +30,33 @@ getColorBtn.addEventListener('click', () => {
         })
         colorContainer.innerHTML = colorHtml
 
+        document.querySelectorAll('[data-color]')
+            .forEach(el=> el.addEventListener('click',() => {
+                //navigator.clipboard.writeText(el.dataset['color'])
+                const copyText = 'copied'
+                const copiedEl = document.querySelector(`[data-tooltip="${el.dataset['color']}"]`)
+                input.value=el.dataset['color']
+                copytext()
+                copiedEl.textContent = copyText
+                setTimeout(()=> copiedEl.textContent = ``, 1000, copiedEl)
+            })
+            )    
+        })
+}
 
-        document.addEventListener('click', (e) => {
-            if(e.target.id === 'rectangle-shape' || e.target.id === 'hex') {
-                colors.forEach(hexEl => {
-                let hexValue = hexEl.hex.value
-                    console.log(hexValue)
-                })
-        
-            }
-        })     
-    })
-    
+
+getColorBtn.addEventListener('click', () => {
+    renderMenu()    
 })
 
+// first run
+renderMenu()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function copytext() {
+    //var selectedValues = getSelectValues(this); // get selected values
+    //input.value = test.join(','); // join them in a comma separated list
+    input.select(); // select offscreen inputs text
+    document.execCommand("copy"); // copy it
+    //this.focus(); // focus back on original, so we don't see any glitches
+} 
 
