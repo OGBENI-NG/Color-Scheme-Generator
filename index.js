a552770ec3d016960ec87a393e31f4caf9526c58
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2022-12-25 12:17:05 +0100

    update copy function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const selectOptionColor = document.querySelector('#select-color-option')
 const getColorBtn = document.querySelector('#get-color-btn')
 const colorContainer = document.querySelector('#color-container')
 const color = document.querySelector('#color')
-const input = document.querySelector("input.copyfrom"); 
+// const input = document.querySelector("input.copyfrom");
 
 const renderMenu = ()=> {
     let colorHtml = ''
@@ -12,51 +12,49 @@ const renderMenu = ()=> {
     )
     .then(res => res.json())
     .then(data => {
-        let colors = data.colors
+        const colors = data.colors
         colors.forEach(colorEl => {
             const newColor = colorEl.hex.value
-            
+
             colorHtml += `
                 <div class="hex-container tooltip" id="copy-area">
-                    <span class="tooltiptext" data-tooltip="${newColor}">${newColor}</span>
-                    <div class="rectangle-shape color-one" 
+                    <span class="tooltiptext hid" data-tooltip="${newColor}">copy: ${newColor}</span>
+                    <div class="rectangle-shape color-one"
                         id="rectangle-shape" style="background:${newColor}">
                     </div>
                     <p class="hex" data-color="${newColor}">${newColor}</p>
                 </div>
-                
+
             `
             return newColor
         })
         colorContainer.innerHTML = colorHtml
 
         document.querySelectorAll('[data-color]')
+
             .forEach(el=> el.addEventListener('click',() => {
-                //navigator.clipboard.writeText(el.dataset['color'])
-                const copyText = 'copied'
-                const copiedEl = document.querySelector(`[data-tooltip="${el.dataset['color']}"]`)
-                input.value=el.dataset['color']
-                copytext()
-                copiedEl.textContent = copyText
-                setTimeout(()=> copiedEl.textContent = "", 1000, copiedEl)
+                if(navigator.clipboard.writeText(el.dataset['color'])) {
+                    tooltipsTxt()
+                    // el.dataset(hidden())
+                }
             })
-            )    
-        })
+        )
+
+        function tooltipsTxt(e, index) {
+            const tooltipHtml = document.querySelectorAll('[data-tooltip]')
+            tooltipHtml.forEach( hex => hex.textContent = 'Copied')
+        }
+
+    })
 }
 
 
 getColorBtn.addEventListener('click', () => {
-    renderMenu()    
+    renderMenu()
 })
 
 // first run
 renderMenu()
 
-function copytext() {
-    //var selectedValues = getSelectValues(this); // get selected values
-    //input.value = test.join(','); // join them in a comma separated list
-    input.select(); // select offscreen inputs text
-    document.execCommand("copy"); // copy it
-    //this.focus(); // focus back on original, so we don't see any glitches
-} 
+
 
